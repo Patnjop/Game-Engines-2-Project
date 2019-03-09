195b8d206c4bc1e426a316c2e7e42d28d98cda8e
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-03-09 22:59:43 +0000

    Models in and camera effects

---
+++ Game-Engines-2-Assignment/Assets/CameraLook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public GameObject CamTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, CamTarget.transform.rotation, Time.deltaTime);
+    }
+}
