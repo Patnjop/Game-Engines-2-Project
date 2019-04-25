3cc07166de4cecb80ad4a15d73999b9e26d1a7e9
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-04-25 22:21:12 +0100

    Swarms

--- Game-Engines-2-Assignment/Assets/NebuController.cs
+++ Game-Engines-2-Assignment/Assets/NebuController.cs
@@ -8,6 +8,10 @@ public class NebuController : MonoBehaviour
     public int wakeDistance;
     public int jitterDistance;
     public Boid b;
+    public GameObject Sentinelles2;
+    public GameObject Sentinelles3;
+    public GameObject Sentinelles4;
+    bool changed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +22,10 @@ public class NebuController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(sentinelles.position, this.transform.position) < wakeDistance && Vector3.Distance(sentinelles.position, this.transform.position) > jitterDistance)
+        if (Vector3.Distance(sentinelles.position, this.transform.position) < wakeDistance && Vector3.Distance(sentinelles.position, this.transform.position) > jitterDistance && changed == false)
         {
             this.GetComponent<FollowPath>().enabled = true;
+            changed = true;
         }
         if (Vector3.Distance(sentinelles.position, this.transform.position) < jitterDistance)
         {
@@ -28,6 +33,9 @@ public class NebuController : MonoBehaviour
             this.GetComponent<JitterWander>().enabled = true;
             b.maxSpeed = 100;
             b.maxForce = 20;
+            Sentinelles2.SetActive(true);
+            Sentinelles3.SetActive(true);
+            Sentinelles4.SetActive(true);
         }
     }
 
